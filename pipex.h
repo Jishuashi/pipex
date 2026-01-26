f582e37ce2b0875ddccea93e058469fde03fe2b4
= <=>
2026-01-26 18:21:52 +0100

    Add all include file

--- pipex.h
+++ pipex.h
@@ -6,11 +6,14 @@
 /*   By: hchartie <hchartie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/26 17:29:36 by hchartie          #+#    #+#             */
-/*   Updated: 2026/01/26 17:30:13 by hchartie         ###   ########.fr       */
+/*   Updated: 2026/01/26 18:18:47 by hchartie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PIPEX_H
 # define PIPEX_H
 # include "libft/libft.h"
+# include <string.h>
+# include <sys/wait.h>
+# include <stdio.h>
 #endif
